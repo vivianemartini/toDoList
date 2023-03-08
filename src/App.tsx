9e0af671b5276a9b9142527175802dd240b5d11a
Viviane Martini <martini.viviane@gmail.com>
2023-03-08 17:38:57 -0300

    add button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from './components/Header';
+import { NewTasks } from './components/NewTasks';
 
 export function App() {
   return (
     <div>
       <Header />
+      <NewTasks />
     </div>
   )
 }
