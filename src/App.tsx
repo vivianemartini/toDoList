99b1d9525b714afc11335c9b7d8866fe5a46f9bf
Viviane Martini <martini.viviane@gmail.com>
2023-03-08 17:54:48 -0300

    add newtasks component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Header } from './components/Header';
 import { NewTasks } from './components/NewTasks';
 
+import styles from './App.module.css'
+
 export function App() {
   return (
     <div>
       <Header />
-      <NewTasks />
+
+      <main className={styles.wrapper}>
+        <NewTasks />
+      </main>
+
     </div>
   )
 }
--- src/components/NewTasks.tsx
+++ src/components/NewTasks.tsx
@@ -3,11 +3,11 @@ import { PlusCircle } from '@phosphor-icons/react'
 
 export function NewTasks(){
     return (
-        <div className={styles.newtasks}>
-            <div>
-                <input type="text" placeholder='Adicione uma nova tarefa'/>
+        <div className={styles.box}>
+            <div >
+                <input className={styles.input} type="text" placeholder='Adicione uma nova tarefa'/>
             </div>
-            <button className={styles.newtasks}>
+            <button className={styles.button}>
                 Criar
                 <PlusCircle size={16} />
             </button>
